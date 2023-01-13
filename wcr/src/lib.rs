a571f1db5ecb15aba5ff8f9aea9ddb7047dc1a79
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-01-13 10:25:52 -0500

    Complete chapter 4 with rusty word count

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -24,45 +24,34 @@ pub fn run(config: Config) -> MyResult<()> {
 
     for filename in &config.files {
         // println!("{}", filename);
-        // TODO: implementation here
-        let mut lines = 0;
-        let mut words = 0;
-        let mut bytes = 0;
-        let mut chars = 0;
 
         match open(&filename) {
-            Ok(mut file) => {
-                let mut line = String::new();
+            Ok(file) => {
+                if let Ok(FileInfo {
+                    num_lines,
+                    num_words,
+                    num_bytes,
+                    num_chars,
+                }) = count(file)
+                {
+                    total_lines += num_lines;
+                    total_words += num_words;
+                    total_bytes += num_bytes;
+                    total_chars += num_chars;
 
-                loop {
-                    let bytes_read = file.read_line(&mut line)?;
-                    if bytes_read == 0 {
-                        break;
-                    }
-                    lines += 1;
-                    bytes += bytes_read;
-                    words += &line.split_whitespace().count();
-                    chars += line.chars().count();
-                    line.clear();
+                    println!(
+                        "{}{}{}{}{}",
+                        format_field(num_lines, config.lines),
+                        format_field(num_words, config.words),
+                        format_field(num_bytes, config.bytes),
+                        format_field(num_chars, config.chars),
+                        if filename == "-" {
+                            "".to_string()
+                        } else {
+                            format!(" {}", filename)
+                        }
+                    )
                 }
-
-                total_lines += lines;
-                total_words += words;
-                total_bytes += bytes;
-                total_chars += chars;
-
-                println!(
-                    "{}{}{}{}{}",
-                    format_field(lines, config.lines),
-                    format_field(words, config.words),
-                    format_field(bytes, config.bytes),
-                    format_field(chars, config.chars),
-                    if filename == "-" {
-                        "".to_string()
-                    } else {
-                        format!(" {}", filename)
-                    }
-                )
             }
             Err(e) => eprintln!("{}: {}", filename, e),
         }
@@ -78,26 +67,6 @@ pub fn run(config: Config) -> MyResult<()> {
         );
     }
     Ok(())
-}
-
-fn format_field(value: usize, show: bool) -> String {
-    if show {
-        format!("{:>8}", value)
-    } else {
-        "".to_string()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::format_field;
-
-    #[test]
-    fn test_format_field() {
-        assert_eq!(format_field(1, false), "");
-        assert_eq!(format_field(3, true), "        3");
-        assert_eq!(format_field(10, true), "       10");
-    }
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -130,17 +99,17 @@ pub fn get_args() -> MyResult<Config> {
                 .help(words_help),
         )
         .arg(
-            Arg::with_name("chars")
-                .short("m")
-                .long("chars")
-                .help(chars_help),
-        )
-        .arg(
             Arg::with_name("bytes")
                 .short("c")
                 .long("bytes")
-                .help(bytes_help)
-                .conflicts_with("chars"),
+                .help(bytes_help),
+        )
+        .arg(
+            Arg::with_name("chars")
+                .short("m")
+                .long("chars")
+                .help(chars_help)
+                .conflicts_with("bytes"),
         )
         .get_matches();
 
@@ -169,9 +138,81 @@ pub fn get_args() -> MyResult<Config> {
     })
 }
 
+#[derive(Debug, PartialEq)]
+pub struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
+}
+
+pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+    let mut num_lines = 0;
+    let mut num_words = 0;
+    let mut num_bytes = 0;
+    let mut num_chars = 0;
+
+    let mut line = String::new();
+
+    loop {
+        let bytes_read = file.read_line(&mut line)?;
+        if bytes_read == 0 {
+            break;
+        }
+        num_lines += 1;
+        num_bytes += bytes_read;
+        num_words += line.split_whitespace().count();
+        num_chars += line.chars().count();
+        line.clear();
+    }
+
+    Ok(FileInfo {
+        num_lines,
+        num_words,
+        num_bytes,
+        num_chars,
+    })
+}
+
 fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     match filename {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
         _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn format_field(value: usize, show: bool) -> String {
+    if show {
+        format!("{:>8}", value)
+    } else {
+        "".to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{count, format_field, FileInfo};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world. I just want your half.\r\n";
+        let info = count(Cursor::new(text));
+        assert!(info.is_ok());
+
+        let expected = FileInfo {
+            num_lines: 1,
+            num_words: 10,
+            num_chars: 48,
+            num_bytes: 48,
+        };
+        assert_eq!(info.unwrap(), expected);
+    }
+
+    #[test]
+    fn test_format_field() {
+        assert_eq!(format_field(1, false), "");
+        assert_eq!(format_field(3, true), "       3");
+        assert_eq!(format_field(10, true), "      10");
     }
 }
