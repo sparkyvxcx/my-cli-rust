96fd57d7f16af8a6d8c7a74954396349ba723708
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-02-02 01:10:42 -0500

    Complete chapter 6 with rusty uniq

--- uniqr/src/lib.rs
+++ uniqr/src/lib.rs
@@ -108,3 +108,45 @@ fn write_output(
     }
     Ok(())
 }
+
+pub fn run_example(config: Config) -> MyResult<()> {
+    let mut file = open(&config.in_file).map_err(|e| format!("{}: {}", config.in_file, e))?;
+    let mut out_file: Box<dyn Write> = match &config.out_file {
+        Some(output_path) => Box::new(File::create(output_path)?),
+        None => Box::new(io::stdout()),
+    };
+    let mut line = String::new();
+    let mut previous = String::new();
+    let mut count: u64 = 0;
+
+    let mut print = |count: u64, text: &str| -> MyResult<()> {
+        if count > 0 {
+            if config.count {
+                write!(out_file, "{:>4} {}", count, text)?;
+            } else {
+                write!(out_file, "{}", text)?;
+            }
+        }
+        Ok(())
+    };
+
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        if bytes == 0 {
+            break;
+        }
+
+        if line.trim_end() != previous.trim_end() {
+            print(count, &previous)?;
+            previous = line.clone();
+            count = 0;
+        }
+
+        count += 1;
+        line.clear();
+    }
+
+    print(count, &previous)?;
+
+    Ok(())
+}
