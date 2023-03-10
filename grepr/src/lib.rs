8695ca4ef01201d03c47358764559acb798248b8
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-03-10 09:44:49 -0500

    Adding command line interface

--- grepr/src/lib.rs
+++ grepr/src/lib.rs
@@ -14,10 +14,68 @@ pub struct Config {
 }
 
 pub fn get_args() -> MyResult<Config> {
-    Ok(Config {})
+    let matches = App::new("grepr")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rusty grep")
+        .arg(
+            Arg::with_name("pattern")
+                .value_name("PATTERN")
+                .help("Search pattern")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("recursive")
+                .help("Recursive search")
+                .short("r")
+                .long("recursive"),
+        )
+        .arg(
+            Arg::with_name("count")
+                .help("Count occurrences")
+                .short("c")
+                .long("count"),
+        )
+        .arg(
+            Arg::with_name("invert")
+                .help("Invert match")
+                .short("v")
+                .long("invert-match"),
+        )
+        .arg(
+            Arg::with_name("insensitive")
+                .help("Case-insensitive")
+                .short("i")
+                .long("insensitive"),
+        )
+        .get_matches();
+
+    let files = matches.values_of_lossy("files").unwrap();
+    let pattern = matches
+        .value_of("pattern")
+        .map(|p| Regex::new(&p).map_err(|_| format!("Invalid pattern \"{}\"", p)))
+        .transpose()?
+        .unwrap();
+    let recursive = matches.is_present("recursive");
+    let invert_match = matches.is_present("invert");
+    let count = matches.is_present("count");
+    Ok(Config {
+        pattern,
+        files,
+        recursive,
+        count,
+        invert_match,
+    })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    println!("{:#?}", config);
     Ok(())
 }
