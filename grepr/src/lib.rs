6cbcd2b185c6ecb69dd01a639bc825213078829c
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-03-16 08:51:41 -0400

    Implement find_files function

--- grepr/src/lib.rs
+++ grepr/src/lib.rs
@@ -1,6 +1,7 @@
 use clap::{App, Arg};
 use regex::{Regex, RegexBuilder};
 use std::error::Error;
+use walkdir::WalkDir;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -35,34 +36,38 @@ pub fn get_args() -> MyResult<Config> {
             Arg::with_name("recursive")
                 .help("Recursive search")
                 .short("r")
-                .long("recursive"),
+                .long("recursive")
+                .takes_value(false),
         )
         .arg(
             Arg::with_name("count")
                 .help("Count occurrences")
                 .short("c")
-                .long("count"),
+                .long("count")
+                .takes_value(false),
         )
         .arg(
             Arg::with_name("invert")
                 .help("Invert match")
                 .short("v")
-                .long("invert-match"),
+                .long("invert-match")
+                .takes_value(false),
         )
         .arg(
             Arg::with_name("insensitive")
                 .help("Case-insensitive")
                 .short("i")
-                .long("insensitive"),
+                .long("insensitive")
+                .takes_value(false),
         )
         .get_matches();
 
     let files = matches.values_of_lossy("files").unwrap();
-    let pattern = matches
-        .value_of("pattern")
-        .map(|p| Regex::new(&p).map_err(|_| format!("Invalid pattern \"{}\"", p)))
-        .transpose()?
-        .unwrap();
+    let pattern = matches.value_of("pattern").unwrap();
+    let pattern = RegexBuilder::new(pattern)
+        .case_insensitive(matches.is_present("insensitive"))
+        .build()
+        .map_err(|_| format!("Invalid pattern \"{}\"", pattern))?;
     let recursive = matches.is_present("recursive");
     let invert_match = matches.is_present("invert");
     let count = matches.is_present("count");
@@ -78,4 +83,91 @@ pub fn get_args() -> MyResult<Config> {
 pub fn run(config: Config) -> MyResult<()> {
     println!("{:#?}", config);
     Ok(())
+}
+
+use std::fs;
+
+fn find_files(paths: &[String], recursive: bool) -> Vec<MyResult<String>> {
+    // unimplemented!()
+    let mut results = vec![];
+    for path in paths {
+        let metadata = match fs::metadata(path) {
+            Ok(m) => m,
+            Err(e) => {
+                results.push(Err(From::from(format!("{}", e))));
+                continue;
+            }
+        };
+        let file_type = metadata.file_type();
+
+        if file_type.is_dir() {
+            if recursive {
+                WalkDir::new(path)
+                    .into_iter()
+                    .filter_map(|e| match e {
+                        Ok(entry) => Some(entry),
+                        Err(_) => None,
+                    })
+                    .filter(|entry| entry.file_type().is_file())
+                    .for_each(|entry| results.push(Ok(entry.path().display().to_string())));
+            } else {
+                results.push(Err(From::from(format!("{} is a directory", path))));
+            }
+        } else if file_type.is_file() {
+            results.push(Ok(path.clone()));
+        }
+    }
+
+    dbg!("{}", &results);
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::find_files;
+    use rand::{distributions::Alphanumeric, Rng};
+
+    #[test]
+    fn test_find_files() {
+        // Verify that the function finds a file known to exist
+        let files = find_files(&["./tests/inputs/fox.txt".to_string()], false);
+        assert_eq!(files.len(), 1);
+        assert_eq!(files[0].as_ref().unwrap(), "./tests/inputs/fox.txt");
+
+        // The function should reject a directory without the recursive option
+        let files = find_files(&["./tests/inputs".to_string()], false);
+        assert_eq!(files.len(), 1);
+        if let Err(e) = &files[0] {
+            assert_eq!(e.to_string(), "./tests/inputs is a directory");
+        }
+
+        // Verify the function recurses to find four files in the directory
+        let res = find_files(&["./tests/inputs".to_string()], true);
+        let mut files: Vec<String> = res
+            .iter()
+            .map(|r| r.as_ref().unwrap().replace("\\", "/"))
+            .collect();
+        files.sort();
+        assert_eq!(files.len(), 4);
+        assert_eq!(
+            files,
+            vec![
+                "./tests/inputs/bustle.txt",
+                "./tests/inputs/empty.txt",
+                "./tests/inputs/fox.txt",
+                "./tests/inputs/nobody.txt"
+            ]
+        );
+
+        // Generate a random string to represent a nonexistent file
+        let bad: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+
+        let files = find_files(&[bad], false);
+        assert_eq!(files.len(), 1);
+        assert!(files[0].is_err());
+    }
 }
