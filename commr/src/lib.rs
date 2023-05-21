503db9a178425780771f6cd193c33cc831dcc2fd
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-05-21 18:52:55 +1000

    Chapter 10: change the column suppression flags to selection flags

--- commr/src/lib.rs
+++ commr/src/lib.rs
@@ -26,6 +26,7 @@ enum Column<'a> {
     Col3(&'a str),
 }
 
+// TODO: change the column suppression flags to selection flags
 pub fn get_args() -> MyResult<Config> {
     let matches = App::new("commr")
         .version("0.1.0")
@@ -46,21 +47,21 @@ pub fn get_args() -> MyResult<Config> {
                 .required(true),
         )
         .arg(
-            Arg::with_name("suppress_col1")
+            Arg::with_name("show_col1")
                 .short("1")
-                .help("Suppress printing of column 1")
+                .help("Show printing of column 1")
                 .takes_value(false),
         )
         .arg(
-            Arg::with_name("suppress_col2")
+            Arg::with_name("show_col2")
                 .short("2")
-                .help("Suppress printing of column 2")
+                .help("Show printing of column 2")
                 .takes_value(false),
         )
         .arg(
-            Arg::with_name("suppress_col3")
+            Arg::with_name("show_col3")
                 .short("3")
-                .help("Suppress printing of column 3")
+                .help("Show printing of column 3")
                 .takes_value(false),
         )
         .arg(
@@ -83,9 +84,9 @@ pub fn get_args() -> MyResult<Config> {
     let file1 = matches.value_of("file1").unwrap().to_owned();
     let file2 = matches.value_of("file2").unwrap().to_owned();
 
-    let show_col1 = !matches.is_present("suppress_col1");
-    let show_col2 = !matches.is_present("suppress_col2");
-    let show_col3 = !matches.is_present("suppress_col3");
+    let show_col1 = matches.is_present("show_col1");
+    let show_col2 = matches.is_present("show_col2");
+    let show_col3 = matches.is_present("show_col3");
 
     let insensitive = matches.is_present("insensitive");
     let delimiter = matches.value_of("delimiter").unwrap().to_owned();
@@ -122,24 +123,24 @@ pub fn run(config: Config) -> MyResult<()> {
         let mut columns = vec![];
         match col {
             Col1(val) => {
-                if config.show_col1 {
+                if config.show_col1 || (!config.show_col2 && !config.show_col3) {
                     columns.push(val);
                 }
             }
             Col2(val) => {
-                if config.show_col2 {
-                    if config.show_col1 {
+                if config.show_col2 || (!config.show_col1 && !config.show_col3) {
+                    if config.show_col1 || (!config.show_col2 && !config.show_col3) {
                         columns.push("");
                     }
                     columns.push(val);
                 }
             }
             Col3(val) => {
-                if config.show_col3 {
-                    if config.show_col1 {
+                if config.show_col3 || (!config.show_col1 && !config.show_col2) {
+                    if config.show_col1 || (!config.show_col2 && !config.show_col3) {
                         columns.push("");
                     }
-                    if config.show_col2 {
+                    if config.show_col2 || (!config.show_col1 && !config.show_col3) {
                         columns.push("");
                     }
                     columns.push(val);
