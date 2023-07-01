c315a8c92ea499919153453b78fdb36385211e8a
Sparkvix <24603835+sparkyvxcx@users.noreply.github.com>
2023-07-01 21:23:02 +1000

    Chapter 12: implement generic parse int function

--- calr/src/lib.rs
+++ calr/src/lib.rs
@@ -1,6 +1,7 @@
-use chrono::NaiveDate;
+use chrono::{Datelike, Local, NaiveDate};
 use clap::{App, Arg};
 use std::error::Error;
+use std::str::FromStr;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -17,11 +18,12 @@ pub fn get_args() -> MyResult<Config> {
         .author("Ken Youens-Clark <kyclark@gmail.com>")
         .about("Rusty cal")
         .arg(
-            Arg::with_name("current_year")
+            Arg::with_name("whole_year")
                 .help("Show whole current year")
                 .short("y")
                 .long("year")
-                .takes_value(false),
+                .takes_value(false)
+                .conflicts_with_all(&["month", "year"]),
         )
         .arg(
             Arg::with_name("month")
@@ -34,18 +36,66 @@ pub fn get_args() -> MyResult<Config> {
             Arg::with_name("year")
                 .help("Year (1-9999)")
                 .value_name("YEAR")
-                .takes_value(true),
+                .takes_value(true)
+                .conflicts_with("month"),
         )
         .get_matches();
 
     // dummy placeholder data
-    let month = Some(1);
-    let year = 1;
-    let today = NaiveDate::parse_from_str(matches.value_of("year").unwrap(), "%Y")?;
-    Ok(Config { month, year, today })
+    let mut month = matches
+        .value_of("month")
+        .map(|m| m.parse::<u32>())
+        .transpose()?;
+    let whole_year = matches.is_present("whole_year");
+    let today = Local::today();
+
+    let mut year = matches
+        .value_of("year")
+        .map(|y| y.parse::<i32>())
+        .transpose()?
+        .unwrap_or(today.year());
+
+    if whole_year {
+        month = None;
+        year = today.year();
+    }
+
+    Ok(Config {
+        month,
+        year,
+        today: today.naive_local(),
+    })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
     println!("{:?}", config);
     Ok(())
+}
+
+fn parse_int<T: FromStr>(val: &str) -> MyResult<T> {
+    val.parse::<T>()
+        .map_err(|_| From::from(format!("Invalid integer \"{}\"", val)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_int;
+
+    #[test]
+    fn test_parse_int() {
+        // Parse positive int as usize
+        let res = parse_int::<usize>("1");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), 1_usize);
+
+        // Parse negative int as i32
+        let res = parse_int::<i32>("-1");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), -1_i32);
+
+        // Fail on a string
+        let res = parse_int::<i64>("foo");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "Invalid integer \"foo\"");
+    }
 }
